9417bcc2a053c592bf408dea9be8737c73f9e525
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-06-09 23:33:37 +0900

    Add db container

---
+++ ddl/ddl.sql
@@ -0,0 +1,156 @@
+-- -----------------------------------------------------
+-- Schema eval-speech
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eval-speech` DEFAULT CHARACTER SET utf8 ;
+USE `eval-speech` ;
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `type` INT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC)
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`teachers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`teachers` (
+  `user_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `gender` INT NULL,
+  `birth_date` DATETIME NULL,
+  `birth_place` INT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `fk_tearcher_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `eval-speech`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`learners`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`learners` (
+  `user_id` INT NOT NULL,
+  `teacher_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `gender` INT NULL,
+  `birth_date` DATETIME NULL,
+  `birth_place` INT NULL,
+  PRIMARY KEY (`user_id`),
+  INDEX `fk_learner_teacher_id_idx` (`teacher_id` ASC),
+  CONSTRAINT `fk_learner_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `eval-speech`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_learner_teacher_id`
+    FOREIGN KEY (`teacher_id`)
+    REFERENCES `eval-speech`.`teachers` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`units`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`units` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `teacher_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `idx_teacher_id` (`teacher_id` ASC),
+  CONSTRAINT `fk_unit_teacher_id`
+    FOREIGN KEY (`id`)
+    REFERENCES `eval-speech`.`teachers` (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`teacher_speeches`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`teacher_speeches` (
+  `id` INT NOT NULL,
+  `teacher_id` INT NOT NULL,
+  `text` VARCHAR(1000) NULL,
+  `object_key` VARCHAR(200) NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_model_speech_teacher_id_idx` (`teacher_id` ASC),
+  CONSTRAINT `fk_model_speech_teacher_id`
+    FOREIGN KEY (`teacher_id`)
+    REFERENCES `eval-speech`.`teachers` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`learner_speeches`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`learner_speeches` (
+  `id` INT NOT NULL,
+  `learner_id` INT NULL,
+  `unit_id` INT NULL,
+  `teacher_speech_id` INT NULL,
+  `type` INT NULL,
+  `object_key` VARCHAR(200) NULL,
+  `gop_average` DOUBLE NULL,
+  `gop_file_key` VARCHAR(200) NULL,
+  `dtw_average` DOUBLE NULL,
+  `dtw_file_key` VARCHAR(200) NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_learner_id_idx` (`learner_id` ASC),
+  INDEX `fk_unit_id_idx` (`unit_id` ASC),
+  INDEX `fk_teacher_id_idx` (`teacher_speech_id` ASC),
+  CONSTRAINT `fk_learner_speech_learner_id`
+    FOREIGN KEY (`learner_id`)
+    REFERENCES `eval-speech`.`learners` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_learner_speech_unit_id`
+    FOREIGN KEY (`unit_id`)
+    REFERENCES `eval-speech`.`units` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_learner_speech_teacher_speech_id`
+    FOREIGN KEY (`teacher_speech_id`)
+    REFERENCES `eval-speech`.`teacher_speeches` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table `eval-speech`.`units_teacher_speeches`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `eval-speech`.`units_teacher_speeches` (
+  `unit_id` INT NOT NULL,
+  `teacher_speech_id` INT NOT NULL,
+  INDEX `fk_md_unit_id_idx` (`unit_id` ASC),
+  INDEX `fk_md_teacher_speech_id_idx` (`teacher_speech_id` ASC),
+  CONSTRAINT `fk_md_unit_id`
+    FOREIGN KEY (`unit_id`)
+    REFERENCES `eval-speech`.`units` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_md_teacher_speech_id`
+    FOREIGN KEY (`teacher_speech_id`)
+    REFERENCES `eval-speech`.`teacher_speeches` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
