6bc8e4f82fd6e871954e71d2829cb10c931c7964
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-06-15 22:12:55 +0900

    Add s3_client

--- api/util/config.py
+++ api/util/config.py
@@ -7,3 +7,7 @@ MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+S3_BUCKET_NAME = 'eval-speech'
