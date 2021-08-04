d054f127300dba319072e59004a8786e4e9932f3
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-08-05 00:28:00 +0900

    Fix terraform network

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
 
   tags = {
     Name = "public-${var.prj_name}"
@@ -20,6 +21,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
 
   tags = {
     Name = "private-${var.prj_name}-1"
