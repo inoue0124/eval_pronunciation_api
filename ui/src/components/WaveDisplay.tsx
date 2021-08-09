961b52e8cac8a13ce5fd1d04e209bcffce8d1cf6
inoue0124 <inoue0124@gavo.t.u-tokyo.ac.jp>
2021-08-09 17:16:13 +0900

    Fix showing wave ui

--- ui/src/components/WaveDisplay.tsx
+++ ui/src/components/WaveDisplay.tsx
@@ -1,98 +1,216 @@
 /* eslint-disable */
 import axios from 'axios'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
 import { Filter } from '../util/Filter'
 import WaveSurfer from 'wavesurfer.js'
 // @ts-expect-error Type-definitions are missing
 import CursorPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.cursor.js'
-// @ts-expect-error Type-definitions are missing
-import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.js'
-import { Button } from '@material-ui/core'
+import { Scatter } from 'react-chartjs-2'
+import {
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Typography,
+  Theme,
+  IconButton,
+} from '@material-ui/core'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import StopIcon from '@material-ui/icons/Stop'
 
-type Props = {
-  url: string
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    waveForm: {
+      position: 'relative',
+      marginLeft: '35px',
+      marginRight: '10px',
+    },
+    indicatorWrapper: {
+      textAlign: 'center',
+      marginTop: theme.spacing(2),
+      marginBottom: -theme.spacing(10),
+    },
+    pitchWrapper: {
+      position: 'relative',
+    },
+  }),
+)
+
+type Data = {
+  x: number
+  y: number
 }
 
-export const WaveDisplay: React.FC<Props> = ({ url }) => {
+type Props = {
+  isDisableToolbar: boolean
+  label: string
+  url: string
+  fn_ref: any
+  onFinishPlaying?: () => void
+}
+export const WaveDisplay: React.FC<Props> = ({
+  isDisableToolbar,
+  label,
+  url,
+  fn_ref,
+  onFinishPlaying,
+}) => {
+  const classes = useStyles()
   const wavesurfer = useRef<WaveSurfer | null>(null)
   const waveformRef = useRef<HTMLDivElement | null>(null)
-  const waveformTimeline = useRef<HTMLDivElement | null>(null)
-
   const workerRef = useRef<Worker>()
+  const [graphData, setGraphData] = useState<any>(undefined)
+  const [graphOptions, setGraphOptions] = useState<any>(undefined)
+  let pitchData: Data[] = []
+  const FXRATE = 200
+  const audioContext = new AudioContext()
+  const samplingRate = audioContext.sampleRate
+  const [isCalculatingPitch, setIsCalculatingPitch] = useState<boolean>(false)
+
+  // ピッチ計算
   useEffect(() => {
+    setIsCalculatingPitch(true)
     workerRef.current = new Worker(new URL('../util/pitch.worker', import.meta.url))
+    // URLから音声データを取得しピッチを抽出
+    axios
+      .get(url, {
+        responseType: 'arraybuffer',
+        headers: {
+          'Content-Type': 'audio/mpeg',
+        },
+      })
+      .then((response) => {
+        audioContext.decodeAudioData(
+          response.data,
+          function onSuccess(buffer: any) {
+            const siglen = buffer.length
+            const fsignal = new Float32Array(siglen)
+            const srcbuf = buffer.getChannelData(0)
+            let sigmax = 0
+            for (let i = 0; i < siglen; i++) {
+              var s = srcbuf[i]
+              if (s > sigmax) sigmax = s
+              if (s < -sigmax) sigmax = -s
+            }
+            for (let i = 0; i < siglen; i++) {
+              fsignal[i] = srcbuf[i] / sigmax
+            }
+            const nfsamp = Math.floor(fsignal.length / 3)
+            const ffsignal = new Float32Array(nfsamp)
+            const filt = new Filter()
+            filt.design(filt.LOW_PASS, 4, samplingRate / 3, samplingRate / 3, samplingRate)
+            for (var i = 0; i < nfsamp; i++) {
+              filt.sample(fsignal[3 * i])
+              filt.sample(fsignal[3 * i + 1])
+              ffsignal[i] = filt.sample(fsignal[3 * i + 2])
+            }
+            // setGraphData({
+            //   datasets: [
+            //     {
+            //       data: [
+            //         {
+            //           x: -10,
+            //           y: 0,
+            //         },
+            //         {
+            //           x: 0,
+            //           y: 200,
+            //         },
+            //         {
+            //           x: 10,
+            //           y: 100,
+            //         },
+            //         {
+            //           x: 0.5,
+            //           y: 150.5,
+            //         },
+            //       ],
+            //       borderColor: 'rgb(75, 192, 192)',
+            //       borderWidth: 1,
+            //     },
+            //   ],
+            // })
+            // setGraphOptions({
+            //   responsive: true,
+            //   maintainAspectRatio: false,
+            //   plugins: {
+            //     legend: {
+            //       display: false,
+            //     },
+            //     tooltip: {
+            //       enabled: false,
+            //     },
+            //   },
+            // })
+            // setIsCalculatingPitch(false)
+            // workerスレッドから終了メッセージを受信した際の動作
+            workerRef.current!.onmessage = (event) => {
+              pitchData = []
+              for (i = 0; i < fsignal.length; i++) {
+                if (event.data.vs[i] > 0.3) {
+                  pitchData.push({ x: i / FXRATE, y: event.data.fx[i] })
+                }
+              }
+              let ymin = 62.5
+              let ymax = 500
+              if (pitchData.length > 0) {
+                let avgPitch = 0
+                for (i = 0; i < pitchData.length; i++) {
+                  avgPitch += pitchData[i].y
+                }
+                avgPitch /= pitchData.length
+                ymin = avgPitch / 2
+                ymax = avgPitch * 1.5
+              }
+              setGraphData({
+                datasets: [
+                  {
+                    data: pitchData,
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 1,
+                  },
+                ],
+              })
+              setGraphOptions({
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                  tooltip: {
+                    enabled: false,
+                  },
+                },
+                scales: {
+                  x: {
+                    min: 0,
+                    max: fsignal.length / samplingRate,
+                  },
+                  y: {
+                    min: Math.floor(ymin),
+                    max: Math.floor(ymax),
+                  },
+                },
+              })
+              setIsCalculatingPitch(false)
+            }
+            // workerスレッドを開始
+            workerRef.current!.postMessage({ signal: ffsignal, srate: samplingRate / 3 })
+          },
+          function onFailure() {
+            console.log('decode AudioData failed')
+          },
+        )
+      })
+      .catch((error) => alert(error))
     return () => {
       workerRef.current!.terminate()
     }
-  }, [])
+  }, [url])
 
-  const [samplingRate, setSamplingRate] = useState<number>(48000)
-  const [audioContext, setAudioContext] = useState<AudioContext>(new AudioContext())
-
-  const dopitch = (fsignal: any, SRATE: any) => {
-    var nfsamp = Math.floor(fsignal.length / 3)
-    var ffsignal = new Float32Array(nfsamp)
-    var filt = new Filter()
-    filt.design(filt.LOW_PASS, 4, SRATE / 3, SRATE / 3, SRATE)
-    for (var i = 0; i < nfsamp; i++) {
-      filt.sample(fsignal[3 * i])
-      filt.sample(fsignal[3 * i + 1])
-      ffsignal[i] = filt.sample(fsignal[3 * i + 2])
-    }
-
-    console.log('HERE')
-
-    workerRef.current!.onmessage = (event) => {
-      const ffx = event.data.fx
-      const fvs = event.data.vs
-      const ffxlen = event.data.fxlen
-      // for (i = 0; i < nsamp; i++) {
-      //   if (fvs[ssamp + i] > 0.3)
-      //     pdata.push([(i + ssamp) / FXRATE, ffx[ssamp + i]]);
-      // }
-      console.log(ffx, fvs, ffxlen)
-    }
-
-    workerRef.current!.postMessage({ signal: ffsignal, srate: samplingRate / 3 })
-  }
-
-  axios
-    .get(url, {
-      responseType: 'arraybuffer',
-      headers: {
-        'Content-Type': 'audio/mpeg',
-      },
-    })
-    .then((response) => {
-      const filedata = response.data
-      const fileblob = new Blob([filedata], { type: 'audio/wav' })
-      const fileurl = URL.createObjectURL(fileblob)
-
-      audioContext.decodeAudioData(
-        filedata,
-        function onSuccess(buffer: any) {
-          let siglen = buffer.length
-          let fsignal = new Float32Array(siglen)
-          let srcbuf = buffer.getChannelData(0)
-          let sigmax = 0
-          for (let i = 0; i < siglen; i++) {
-            var s = srcbuf[i]
-            if (s > sigmax) sigmax = s
-            if (s < -sigmax) sigmax = -s
-          }
-          for (let i = 0; i < siglen; i++) fsignal[i] = srcbuf[i] / sigmax
-
-          dopitch(fsignal, samplingRate)
-        },
-        function onFailure() {
-          alert('decodeAudioData failed')
-        },
-      )
-    })
-    .catch((error) => console.log(error))
-
+  // 音声波形表示
   useEffect(() => {
-    if (waveformRef.current && waveformTimeline.current) {
+    if (waveformRef.current) {
       wavesurfer.current = WaveSurfer.create({
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         container: waveformRef.current!,
@@ -107,27 +225,71 @@ export const WaveDisplay: React.FC<Props> = ({ url }) => {
               padding: '2px',
             },
           }),
-          TimelinePlugin.create({
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            container: waveformTimeline.current!,
-          }),
         ],
       })
-
       wavesurfer.current.load(url)
-
       return () => {
         wavesurfer.current?.destroy()
       }
-    } else {
-      return undefined
     }
-  }, [url, waveformRef, waveformTimeline])
+  }, [url, waveformRef])
+
+  useEffect(() => {
+    if (wavesurfer.current) {
+      wavesurfer.current.unAll() // finishのイベントが重複して付いてしまうので毎回消す
+      wavesurfer.current.once('finish', () => {
+        wavesurfer.current?.seekTo(0)
+        if (onFinishPlaying) onFinishPlaying()
+      })
+    }
+  }, [onFinishPlaying])
+
+  fn_ref.current = {
+    play: () => {
+      wavesurfer.current?.seekTo(0)
+      wavesurfer.current?.play()
+    },
+    stop: () => wavesurfer.current?.stop(),
+  }
+
+  const PitchGraph = useMemo(() => {
+    return <Scatter data={graphData} options={graphOptions} height={30} />
+  }, [graphData, graphOptions])
 
   return (
     <div>
-      <div id="waveform" ref={waveformRef} style={{ position: 'relative' }} />
-      <div id="waveform-timeline" ref={waveformTimeline} />
+      <Typography color="textPrimary" variant="h6" display="inline">
+        {label}
+      </Typography>
+      <IconButton
+        disabled={isDisableToolbar}
+        onClick={() => {
+          wavesurfer.current?.play()
+        }}
+      >
+        <PlayArrowIcon />
+      </IconButton>
+      <IconButton
+        disabled={isDisableToolbar}
+        onClick={() => {
+          wavesurfer.current?.stop()
+        }}
+      >
+        <StopIcon />
+      </IconButton>
+      <div ref={waveformRef} className={classes.waveForm} />
+
+      <div className={classes.pitchWrapper}>
+        {isCalculatingPitch && (
+          <div className={classes.indicatorWrapper}>
+            <CircularProgress />
+            <Typography variant="caption" display="block" gutterBottom>
+              ピッチ計算中
+            </Typography>
+          </div>
+        )}
+        {PitchGraph}
+      </div>
     </div>
   )
 }
--- ui/src/pages/learner/unit/[id].tsx
+++ ui/src/pages/learner/unit/[id].tsx
@@ -1,23 +1,52 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
+import { useReactMediaRecorder } from 'react-media-recorder'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import { Card, CardContent, createStyles, makeStyles, Theme, Typography } from '@material-ui/core'
+import {
+  Button,
+  Card,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+  LinearProgress,
+} from '@material-ui/core'
 import ApiClient from '../../../api'
 import { Unit } from '../../../types/Unit'
 import { getCookie } from '../../../util/cookie'
 import { NextPage } from 'next'
 import { User } from '../../../types/User'
+import MicIcon from '@material-ui/icons/Mic'
 const WaveDisplay = dynamic<any>(
   () => import('../../../components/WaveDisplay').then((module) => module.WaveDisplay),
   { ssr: false },
 )
 
+function usePrevious(value: any) {
+  const ref = useRef(null)
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     card: {
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(2),
+      margin: theme.spacing(2),
+      padding: theme.spacing(2),
+    },
+    cardTitle: {
+      marginBottom: theme.spacing(1),
+    },
+    toolbarButton: {
+      marginRight: theme.spacing(1),
+    },
+    recordingIndicatorWrapper: {
+      textAlign: 'center',
+      margin: 'auto',
+      width: '100px',
     },
   }),
 )
@@ -30,6 +59,17 @@ const UnitDetail: NextPage = () => {
   const teacherId = Number(router.query.ti)
   const [user, setUser] = useState<User>()
   const [unit, setUnit] = useState<Unit>()
+  const [isRepeating, setIsRepeating] = useState<boolean>(true)
+  const [isRecording, setIsRecording] = useState<boolean>(false)
+  const [isShowText, setIsShowText] = useState<boolean>(false)
+  const [speechIndex, setSpeechIndex] = useState<number>(0)
+  const teacherWavRef = useRef<any>(null)
+  const learnerWavRef = useRef<any>(null)
+  const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({
+    audio: true,
+  })
+  const prevMediaBlobUrl = usePrevious(mediaBlobUrl)
+
   useEffect(() => {
     if (router.isReady) {
       const logged_user = getCookie().logged_user
@@ -51,21 +91,135 @@ const UnitDetail: NextPage = () => {
     }
   }, [router.query])
 
+  const onClickStartRepeating = () => {
+    setIsRepeating(true)
+    setIsRecording(true)
+    teacherWavRef.current!.play()
+  }
+  const onClickStartShadowing = () => {
+    setIsRepeating(false)
+    startRecording()
+    setIsRecording(true)
+    teacherWavRef.current!.play()
+  }
+  const onClickStopRecording = () => {
+    stopRecording()
+    setIsRecording(false)
+    teacherWavRef.current!.stop()
+  }
+  const onClickShowText = () => {
+    setIsShowText(!isShowText)
+  }
+  const onClickNext = () => {
+    setSpeechIndex(speechIndex + 1)
+  }
+
+  const LearnerWav = useMemo(() => {
+    return (
+      <Card className={classes.card}>
+        <WaveDisplay
+          isDisabled={isRecording}
+          label="あなたの音声"
+          fn_ref={learnerWavRef}
+          url={mediaBlobUrl}
+        />
+      </Card>
+    )
+  }, [mediaBlobUrl])
+
   return (
     <>
       {unit && (
-        <Card className={classes.card}>
-          <CardContent>
+        <>
+          <Card className={classes.card}>
             <Typography color="textPrimary">課題ID：{unit.id}</Typography>
             <Typography color="textPrimary">課題名：{unit.name}</Typography>
-            <Typography color="textPrimary">
+            <Typography color="textPrimary" style={{ marginBottom: '10px' }}>
               作成日時：{new Date(unit.created_at).toLocaleString()}
             </Typography>
-          </CardContent>
-          <WaveDisplay
-            url={'https://eval-speech.s3.ap-northeast-1.amazonaws.com/shadowing_2.MP3'}
-          />
-        </Card>
+            <Button
+              className={classes.toolbarButton}
+              variant="contained"
+              color="primary"
+              onClick={onClickStartRepeating}
+              disableElevation
+              disabled={isRecording}
+            >
+              リピーティング開始
+            </Button>
+            <Button
+              className={classes.toolbarButton}
+              variant="contained"
+              color="primary"
+              onClick={onClickStartShadowing}
+              disableElevation
+              disabled={isRecording}
+            >
+              シャドーイング開始
+            </Button>
+            <Button
+              className={classes.toolbarButton}
+              variant="contained"
+              color="primary"
+              onClick={onClickStopRecording}
+              disableElevation
+              disabled={!isRecording}
+            >
+              録音停止
+            </Button>
+            <Button
+              className={classes.toolbarButton}
+              variant="contained"
+              color="primary"
+              onClick={onClickShowText}
+              disableElevation
+            >
+              テキスト表示
+            </Button>
+            <Button
+              className={classes.toolbarButton}
+              variant="contained"
+              color="primary"
+              onClick={onClickNext}
+              disableElevation
+              disabled={isRecording}
+            >
+              次の課題へ
+            </Button>
+          </Card>
+          {isShowText && (
+            <Card className={classes.card}>
+              <Typography color="textPrimary" variant="h5" align="center">
+                {unit.teacher_speeches[speechIndex].text}
+              </Typography>
+            </Card>
+          )}
+          <Card className={classes.card}>
+            <WaveDisplay
+              isDisabled={isRecording}
+              label="先生の音声"
+              fn_ref={teacherWavRef}
+              url={'https://eval-speech.s3.ap-northeast-1.amazonaws.com/shadowing_2.MP3'}
+              onFinishPlaying={isRepeating ? startRecording : stopRecording}
+            />
+          </Card>
+
+          {mediaBlobUrl && mediaBlobUrl !== prevMediaBlobUrl && !isRecording && (
+            <Card className={classes.card}>
+              <WaveDisplay label="あなたの音声" fn_ref={learnerWavRef} url={mediaBlobUrl} />
+            </Card>
+          )}
+          {mediaBlobUrl && mediaBlobUrl === prevMediaBlobUrl && !isRecording && LearnerWav}
+          {isRecording && (
+            <Card className={classes.recordingIndicatorWrapper}>
+              <MicIcon style={{ fontSize: 40 }} />
+              <Typography variant="caption" display="block" gutterBottom>
+                録音中
+              </Typography>
+              <LinearProgress color="secondary" />
+            </Card>
+          )}
+        </>
       )}
     </>
   )
